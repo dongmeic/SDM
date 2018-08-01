809212b4f262f629c551ce818534ca43be285ce6
dongmeijane1107@gmail.com <dongmeijane1107@gmail.com>
2018-08-01 12:29:22 -0700

    Plot and map the impacts of predictors on the beetle probability

--- R/logistic_model_output.R
+++ R/logistic_model_output.R
@@ -11,8 +11,9 @@ library(latticeExtra)
 library(gridExtra)
 library(RColorBrewer)
 library(animation)
-install.packages("classInt", repos='http://cran.us.r-project.org')
+#install.packages("classInt", repos='http://cran.us.r-project.org')
 library(classInt)
+years <- 1998:2015
 
 DATA_DIR <- '/gpfs/projects/gavingrp/dongmeic/beetle/output/tables/Xy_internal_split_data'
 setwd('/gpfs/projects/gavingrp/dongmeic/beetle/output/plots/glm')
@@ -91,7 +92,7 @@ all(coeffs$predictor == medians$predictor)
 coeffs$cons <- coeffs$coef * medians$median; coeffs$cons
 # use one year data
 #data2 <- read.csv("/gpfs/projects/gavingrp/dongmeic/beetle/output/tables/input_data_2009.csv", stringsAsFactors = FALSE)
-data1 <- data[data$year==2009,]
+#data1 <- data[data$year==2008,]
 
 shppath <- "/gpfs/projects/gavingrp/dongmeic/beetle/shapefiles"
 canada.prov <- readOGR(dsn = shppath, layer = "na10km_can_prov")
@@ -99,15 +100,7 @@ us.states <- readOGR(dsn = shppath, layer = "na10km_us_state")
 crs <- proj4string(us.states)
 lrglakes <- readOGR(dsn = shppath, layer = "na10km_lrglakes")
 proj4string(lrglakes) <- crs
-
 lonlat <- CRS("+proj=longlat +datum=NAD83")
-df <- data1
-xy <- data.frame(df[,c(4,5)])
-coordinates(xy) <- c('lon', 'lat')
-proj4string(xy) <- lonlat
-spdf <- SpatialPointsDataFrame(coords = xy, data = df, proj4string = lonlat)
-spdf <- spTransform(spdf, crs)
-spdf1 <- spdf[spdf$btl_t==1,]
 
 nclr <- 5
 color <- "RdYlGn"
@@ -117,13 +110,13 @@ varnames <- c('Minimum winter temperature', 'Day degrees', 'Mean temperature in 
 							'Summer precipitation', 'Maximum August temperature', 'Minimum October temperature', 
 							'Water-year mean temperature', 'Precipitation from March to August')
 							
-probmapping <- function(var){
-	plotvar <- prob.df[,var]
+probmapping <- function(var,year){
+	plotvar <- prob.df2[,var]
 	class <- classIntervals(plotvar, nclr, style="kmeans", dataPrecision=2)
 	colcode <- findColours(class, plotclr)
-	png(paste0(var,"_map.png"), width=7, height=5, units="in", res=300)
+	png(paste0(var,"_",year,"_map.png"), width=7, height=5, units="in", res=300)
 	par(mfrow=c(1,1),mar=c(0.5,8,1.5,0))
-	plot(spdf, col=colcode, main="Beetle probability predicted", pch=19, cex=0.1)
+	plot(spdf, col=colcode, main=paste("Beetle probability predicted in", year), pch=19, cex=0.1)
 	plot(spdf1, pch=19, cex=0.1, col=rgb(0,0,1,0.1),add=T)
 	#plot(canada.prov, col=rgb(0.7,0.7,0.7,0.7), add=T)
 	#plot(us.states, col=rgb(0.7,0.7,0.7,0.7), add=T)
@@ -137,63 +130,218 @@ probmapping <- function(var){
 probplotting <- function(var){
 		x <- data1[,var]
 		y1 <- prob.df[,var]
-		png(paste0(var,".png"), width=6, height=5, units="in", res=300)
+		png(paste0(var,"_plot.png"), width=6, height=5, units="in", res=300)
 		par(mfrow=c(1,1),mar=c(4.5,4.5,1,1))
-		plot(x, y1, xlab=varnames[which(vars==var)], ylab="Beetle probability", pch=19, cex=0.05)
+		plot(x, y1, xlab=varnames[which(vars==var)], ylab="Beetle probability", pch=19, cex=0.5)
 		dev.off()
 }
 
-# test on minimum winter temperature
-x <- data1$winterTmin
-y <- sum(coeffs$cons)-0.315128553*scale(x^2)-1.72935019-0.1069022594
-y1 <- exp(y)/(1+exp(y))
-dev.off()
-# ddAugJul
-x <- data1$ddAugJul
-y <- sum(coeffs$cons)-0.195761379*scale(x)-1.72935019-0.0595355921
-y2 <- exp(y)/(1+exp(y))
-#hist(dt[dt$btl_t == 1,]$ddAugJul)
-# fallTmean
-x <- data1$fallTmean
-y <- sum(coeffs$cons)-0.104098872*scale(x^2)-0.060015740*scale(x*median(data1$summerP1))-0.014053525*scale(x*median(data1$Tmean))-1.72935019-0.0377606870-0.0141800769-0.0051272637
-y3 <- exp(y)/(1+exp(y))
-#hist(dt[dt$btl_t == 1,]$fallTmean)
-# age
-x <- data1$age
-y <- sum(coeffs$cons)+0.050150919*scale(x)+0.0342156105-1.72935019
-y4 <- exp(y)/(1+exp(y))
-# density
-x <- data1$density
-y <- sum(coeffs$cons)+0.114451743*scale(x*median(data1$TOctSep))+0.028270702*scale(x*median(data1$AugTmax))-0.009827704*scale(x*median(data1$OctTmin))+0.0346628659+0.0078639212+0.0018600826-1.72935019
-y5 <- exp(y)/(1+exp(y))
-# summerP1
-x <- data1$summerP1
-y <- sum(coeffs$cons)-0.060015740*scale(x*median(data1$fallTmean))-0.034204228*scale(x*median(data1$AugTmax))-0.0141800769-0.0005781264-1.72935019
-y6 <- exp(y)/(1+exp(y))
-# AugTmax
-x <- data1$AugTmax
-y <- sum(coeffs$cons)-0.034204228*scale(x*median(data1$summerP1))+0.028270702*scale(x*median(data1$density, na.rm=T))-0.0005781264+0.0078639212-1.72935019
-y7 <- exp(y)/(1+exp(y))
-# OctTmin
-x <- data1$OctTmin
-y <- sum(coeffs$cons)-0.062630669*scale(x)-0.009827704*scale(x*median(data1$density, na.rm=T))-1.72935019-0.0146391775+0.0018600826
-y8 <- exp(y)/(1+exp(y))
-# TOctSep
-x <- data1$TOctSep
-y <- sum(coeffs$cons)+0.114451743*scale(x*median(data1$density, na.rm=T))+0.0346628659-1.72935019
-y9 <- exp(y)/(1+exp(y))
-#PMarAug
-x <- data1$PMarAug
-y <- sum(coeffs$cons)-0.044948108*scale(x^2)-1.72935019-0.0097848475
-y10 <- exp(y)/(1+exp(y))
-prob.df <- data.frame(cbind(y1,y2,y3,y4,y5,y6,y7,y8,y9,y10))
-colnames(prob.df) <- vars
-
-# make plots and maps
-for(i in 1:length(vars)){
-	probplotting(vars[i])
-	probmapping(vars[i])
-	cat(sprintf('Completed %s ...\n', vars[i]))
+prob.plt.scaled <- function(var){
+		x <- xdf[,var]
+		y1 <- prob.df[,var]
+		png(paste0(var,"_x-scaled.png"), width=6, height=5, units="in", res=300)
+		par(mfrow=c(1,1),mar=c(4.5,4.5,1,1))
+		plot(x, y1, xlab=varnames[which(vars==var)], ylab="Beetle probability", pch=19, cex=0.5)
+		dev.off()
 }
 
+predicted <- function(
+  sum9_t1=coeffs$cons[1],vgt=coeffs$cons[2],sum9_t2=coeffs$cons[3],winterTmin_sq=coeffs$cons[4],
+  btl_t2=coeffs$cons[5],btl_t1=coeffs$cons[6],ddAugJul=coeffs$cons[7],density_TOctSep=coeffs$cons[8],
+	fallTmean_sq=coeffs$cons[9],OctTmin=coeffs$cons[10],fallTmean_summerP1=coeffs$cons[11],
+	lat_etopo1=coeffs$cons[12],age=coeffs$cons[13],PMarAug_sq=coeffs$cons[14],AugTmax_summerP1=coeffs$cons[15],
+	density_AugTmax=coeffs$cons[16],lon_sq=coeffs$cons[17],fallTmean_Tmean=coeffs$cons[18],
+	density_OctTmin=coeffs$cons[19],lon=coeffs$cons[20],JanTmin_PPT=coeffs$cons[21],MarTmin_TOctSep=coeffs$cons[22]){
+  intercept <- -1.72935019
+  pred.y <- intercept+sum9_t1+vgt+sum9_t2+winterTmin_sq+btl_t2+btl_t1+ddAugJul+density_TOctSep+fallTmean_sq+OctTmin+
+  					fallTmean_summerP1+lat_etopo1+age+PMarAug_sq+AugTmax_summerP1+density_AugTmax+lon_sq+fallTmean_Tmean+
+  					density_OctTmin+lon+JanTmin_PPT+MarTmin_TOctSep
+  return(pred.y)	   
+ }
+
+# get y dataframe by year
+get.y <- function(){
+	# test on minimum winter temperature
+	x <- data1$winterTmin
+	y <- predicted(winterTmin_sq=coeffs$coef[4]*scale(x^2))
+	y1 <- exp(y)/(1+exp(y))
+	# ddAugJul
+	x <- data1$ddAugJul
+	y <- predicted(ddAugJul=coeffs$coef[7]*scale(x))
+	y2 <- exp(y)/(1+exp(y))
+	#hist(dt[dt$btl_t == 1,]$ddAugJul)
+	# fallTmean
+	x <- data1$fallTmean
+	y <- predicted(fallTmean_sq=coeffs$coef[9]*scale(x^2),
+								 fallTmean_summerP1=coeffs$coef[11]*scale(x*median(data1$summerP1,na.rm=T)),
+								 fallTmean_Tmean=coeffs$coef[18]*scale(x*median(data1$Tmean,na.rm=T)))
+	y3 <- exp(y)/(1+exp(y))
+	#hist(dt[dt$btl_t == 1,]$fallTmean)
+	# age
+	x <- data1$age
+	y <- predicted(age=coeffs$coef[13]*scale(x))
+	y4 <- exp(y)/(1+exp(y))
+	# density
+	x <- data1$density
+	y <- predicted(density_TOctSep=coeffs$coef[8]*scale(x*median(data1$TOctSep,na.rm=T)),
+								 density_AugTmax=coeffs$coef[16]*scale(x*median(data1$AugTmax,na.rm=T)),
+								 density_OctTmin=coeffs$coef[19]*scale(x*median(data1$OctTmin,na.rm=T)))
+	y5 <- exp(y)/(1+exp(y))
+	# summerP1
+	x <- data1$summerP1
+	y <- predicted(fallTmean_summerP1=coeffs$coef[11]*scale(x*median(data1$fallTmean,na.rm=T)),
+								 AugTmax_summerP1=coeffs$coef[15]*scale(x*median(data1$AugTmax,na.rm=T)))
+	y6 <- exp(y)/(1+exp(y))
+	# AugTmax
+	x <- data1$AugTmax
+	y <- predicted(density_AugTmax=coeffs$coef[16]*scale(x*median(data1$density,na.rm=T)),
+								 AugTmax_summerP1=coeffs$coef[15]*scale(x*median(data1$summerP1,na.rm=T)))
+	y7 <- exp(y)/(1+exp(y))
+	# OctTmin
+	x <- data1$OctTmin
+	y <- predicted(OctTmin=coeffs$coef[10]*scale(x),
+								 density_OctTmin=coeffs$coef[19]*scale(x*median(data1$density,na.rm=T)))
+	y8 <- exp(y)/(1+exp(y))
+	# TOctSep
+	x <- data1$TOctSep
+	y <- predicted(density_TOctSep=coeffs$coef[8]*scale(x*median(data1$density,na.rm=T)),
+								 MarTmin_TOctSep=coeffs$coef[22]*scale(x*median(data1$MarTmin,na.rm=T)))
+	y9 <- exp(y)/(1+exp(y))
+	#PMarAug
+	x <- data1$PMarAug
+	y <- predicted(PMarAug_sq=coeffs$coef[14]*scale(x^2))
+	y10 <- exp(y)/(1+exp(y))
+	prob.df <- data.frame(cbind(y1,y2,y3,y4,y5,y6,y7,y8,y9,y10))
+	colnames(prob.df) <- vars
+	return(prob.df)
+}
+
+get.yy <- function(){
+	# test on minimum winter temperature
+	x <- data1$winterTmin
+	y <- predicted(vgt=coeffs$coef[2]*scale(data1$vgt),
+								lat_etopo1=coeffs$coef[12]*scale(data1$lat*data1$etopo1),
+								lon_sq=coeffs$coef[17]*scale(data1$lon^2),
+								lon=coeffs$coef[20]*scale(data1$lon),
+								winterTmin_sq=coeffs$coef[4]*scale(x^2))
+	y1 <- exp(y)/(1+exp(y))
+	# ddAugJul
+	x <- data1$ddAugJul
+	y <- predicted(vgt=coeffs$coef[2]*scale(data1$vgt),
+								lat_etopo1=coeffs$coef[12]*scale(data1$lat*data1$etopo1),
+								lon_sq=coeffs$coef[17]*scale(data1$lon^2),
+								lon=coeffs$coef[20]*scale(data1$lon),
+								ddAugJul=coeffs$coef[7]*scale(x))
+	y2 <- exp(y)/(1+exp(y))
+	#hist(dt[dt$btl_t == 1,]$ddAugJul)
+	# fallTmean
+	x <- data1$fallTmean
+	y <- predicted(vgt=coeffs$coef[2]*scale(data1$vgt),
+								lat_etopo1=coeffs$coef[12]*scale(data1$lat*data1$etopo1),
+								lon_sq=coeffs$coef[17]*scale(data1$lon^2),
+								lon=coeffs$coef[20]*scale(data1$lon),
+								fallTmean_sq=coeffs$coef[9]*scale(x^2),
+								fallTmean_summerP1=coeffs$coef[11]*scale(x*median(data1$summerP1,na.rm=T)),
+								fallTmean_Tmean=coeffs$coef[18]*scale(x*median(data1$Tmean,na.rm=T)))
+	y3 <- exp(y)/(1+exp(y))
+	#hist(dt[dt$btl_t == 1,]$fallTmean)
+	# age
+	x <- data1$age
+	y <- predicted(vgt=coeffs$coef[2]*scale(data1$vgt),
+								lat_etopo1=coeffs$coef[12]*scale(data1$lat*data1$etopo1),
+								lon_sq=coeffs$coef[17]*scale(data1$lon^2),
+								lon=coeffs$coef[20]*scale(data1$lon),
+								age=coeffs$coef[13]*scale(x))
+	y4 <- exp(y)/(1+exp(y))
+	# density
+	x <- data1$density
+	y <- predicted(vgt=coeffs$coef[2]*scale(data1$vgt),
+								lat_etopo1=coeffs$coef[12]*scale(data1$lat*data1$etopo1),
+								lon_sq=coeffs$coef[17]*scale(data1$lon^2),
+								lon=coeffs$coef[20]*scale(data1$lon),
+								density_TOctSep=coeffs$coef[8]*scale(x*median(data1$TOctSep,na.rm=T)),
+								density_AugTmax=coeffs$coef[16]*scale(x*median(data1$AugTmax,na.rm=T)),
+								density_OctTmin=coeffs$coef[19]*scale(x*median(data1$OctTmin,na.rm=T)))
+	y5 <- exp(y)/(1+exp(y))
+	# summerP1
+	x <- data1$summerP1
+	y <- predicted(vgt=coeffs$coef[2]*scale(data1$vgt),
+								lat_etopo1=coeffs$coef[12]*scale(data1$lat*data1$etopo1),
+								lon_sq=coeffs$coef[17]*scale(data1$lon^2),
+								lon=coeffs$coef[20]*scale(data1$lon),
+								fallTmean_summerP1=coeffs$coef[11]*scale(x*median(data1$fallTmean,na.rm=T)),
+								AugTmax_summerP1=coeffs$coef[15]*scale(x*median(data1$AugTmax,na.rm=T)))
+	y6 <- exp(y)/(1+exp(y))
+	# AugTmax
+	x <- data1$AugTmax
+	y <- predicted(vgt=coeffs$coef[2]*scale(data1$vgt),
+								lat_etopo1=coeffs$coef[12]*scale(data1$lat*data1$etopo1),
+								lon_sq=coeffs$coef[17]*scale(data1$lon^2),
+								lon=coeffs$coef[20]*scale(data1$lon),
+								density_AugTmax=coeffs$coef[16]*scale(x*median(data1$density,na.rm=T)),
+								AugTmax_summerP1=coeffs$coef[15]*scale(x*median(data1$summerP1,na.rm=T)))
+	y7 <- exp(y)/(1+exp(y))
+	# OctTmin
+	x <- data1$OctTmin
+	y <- predicted(vgt=coeffs$coef[2]*scale(data1$vgt),
+								lat_etopo1=coeffs$coef[12]*scale(data1$lat*data1$etopo1),
+								lon_sq=coeffs$coef[17]*scale(data1$lon^2),
+								lon=coeffs$coef[20]*scale(data1$lon),
+								OctTmin=coeffs$coef[10]*scale(x),
+								density_OctTmin=coeffs$coef[19]*scale(x*median(data1$density,na.rm=T)))
+	y8 <- exp(y)/(1+exp(y))
+	# TOctSep
+	x <- data1$TOctSep
+	y <- predicted(vgt=coeffs$coef[2]*scale(data1$vgt),
+								lat_etopo1=coeffs$coef[12]*scale(data1$lat*data1$etopo1),
+								lon_sq=coeffs$coef[17]*scale(data1$lon^2),
+								lon=coeffs$coef[20]*scale(data1$lon),
+								density_TOctSep=coeffs$coef[8]*scale(x*median(data1$density,na.rm=T)),
+								MarTmin_TOctSep=coeffs$coef[22]*scale(x*median(data1$MarTmin,na.rm=T)))
+	y9 <- exp(y)/(1+exp(y))
+	#PMarAug
+	x <- data1$PMarAug
+	y <- predicted(vgt=coeffs$coef[2]*scale(data1$vgt),
+								lat_etopo1=coeffs$coef[12]*scale(data1$lat*data1$etopo1),
+								lon_sq=coeffs$coef[17]*scale(data1$lon^2),
+								lon=coeffs$coef[20]*scale(data1$lon),
+								PMarAug_sq=coeffs$coef[14]*scale(x^2))
+	y10 <- exp(y)/(1+exp(y))
+	prob.df <- data.frame(cbind(y1,y2,y3,y4,y5,y6,y7,y8,y9,y10))
+	colnames(prob.df) <- vars
+	return(prob.df)
+}
+
+get.spdf <- function(){
+		df <- data1
+		xy <- data.frame(df[,c(4,5)])
+		coordinates(xy) <- c('lon', 'lat')
+		proj4string(xy) <- lonlat
+		spdf <- SpatialPointsDataFrame(coords = xy, data = df, proj4string = lonlat)
+		spdf <- spTransform(spdf, crs)
+		return(spdf)
+}
+
+# make plots and maps
+for (yr in 1:length(years)){
+	data1 <- data[data$year==years[yr],]
+	prob.df <- get.y()
+	prob.df2 <- get.yy()
+	spdf <- get.spdf()
+	spdf1 <- spdf[spdf$btl_t==1,]
+	xdf <- scale(data1[,vars])
+	for(i in 1:length(vars)){
+		if (yr==which(years==2008)){
+			probplotting(vars[i])
+			prob.plt.scaled(vars[i])	
+		}
+		probmapping(vars[i], years[yr])		
+		cat(sprintf('Completed %s in %s...\n', vars[i], years[yr]))
+	}
+	print(paste("Year", years[yr], "is done!"))
+}
+
+for(i in 1:length(vars)){
+	im.convert(paste0(vars[i],"_*_map.png"),output=paste0(vars[i],"_maps.gif"))
+}
 print("all done!")
