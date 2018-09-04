b8f52fd0ecd9e2e5b2b3b909a0bd032677593370
dongmeijane1107@gmail.com <dongmeijane1107@gmail.com>
2018-09-04 08:52:04 -0700

    created tmean and wrote into raster files

--- R/daymet_ncfile.R
+++ R/daymet_ncfile.R
@@ -1,4 +1,3 @@
-library(ncdf4)
 library(raster)
 library(rgdal)
 
@@ -7,7 +6,7 @@ vtype <- "tmax"
 year <- 1996
 doy <- 365
 infolder <- paste0("/gpfs/projects/gavingrp/dongmeic/daymet/", year, "/", vtype, "/")
-file <- paste0(vtype, year %% 100, doy, ".tif")
+file <- paste0(vtype, year %% 100, formatC(doy, width = 3, format = "d", flag = "0"), ".tif")
 indata <- paste0(infolder, file)
 na10km <- "+proj=laea +lat_0=50 +lon_0=-100 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
 
