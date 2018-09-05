3f2eeee2be817f8832112ee6d63d003d6ffb8077
dongmeijane1107@gmail.com <dongmeijane1107@gmail.com>
2018-09-05 02:14:40 -0700

    first loop through doy to get daily mean temperature

---
+++ R/daymet_tmean.R
@@ -0,0 +1,27 @@
+library(raster)
+library(rgdal)
+install.packages("tiff", repos='http://cran.us.r-project.org')
+library(tiff)
+
+read.tif <- function(vtype, year, doy){
+	infolder <- paste0("/gpfs/projects/gavingrp/dongmeic/daymet/", year, "/", vtype, "/")
+	file <- paste0(vtype, year %% 100, formatC(doy, width = 3, format = "d", flag = "0"), ".tif")
+	indata <- paste0(infolder, file)
+	raster(indata)
+}
+
+ptm <- proc.time()
+for(year in 1997:2015){ # run 1996 mannually first
+	infolder <- paste0("/gpfs/projects/gavingrp/dongmeic/daymet/", year, "/tmean/")
+	dir.create(file.path(infolder), showWarnings = FALSE)
+	for(doy in 1:365){
+		tmax <- read.tif("tmax", year, doy)
+		tmin <- read.tif("tmin", year, doy)
+		tmean <- (tmax + tmin)/2	
+		file <- paste0("tmean", year %% 100, formatC(doy, width = 3, format = "d", flag = "0"), ".tif")
+		writeRaster(tmean, filename=paste0(infolder, file), datatype='INT4S', overwrite=TRUE)
+		print(doy)
+	}
+	print(paste("processed",year,"..."))
+}
+proc.time() - ptm
