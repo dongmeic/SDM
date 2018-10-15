deb0fd45753fa06083e506d9ee57565828ff6e42
dongmeijane1107@gmail.com <dongmeijane1107@gmail.com>
2018-10-15 16:52:14 -0700

    check the extents before getting daily mean temperature

--- R/daymet_tmean.R
+++ R/daymet_tmean.R
@@ -18,10 +18,10 @@ read.tif <- function(vtype, year, doy){
 }
 
 ptm <- proc.time()
-for(year in c(1990:1995,1999:2015)){
+for(year in c(1992:1995,1999:2015)){
 	infolder <- paste0("/gpfs/projects/gavingrp/dongmeic/daymet/", year, "/tmean/")
 	dir.create(file.path(infolder), showWarnings = FALSE)
-	for(doy in 260:365){
+	for(doy in 1:365){
 		if(!file.exists(daymet("tmax", year, doy)) | !file.exists(daymet("tmin", year, doy))){
 			if(!file.exists(daymet("tmax", year, doy))){
 				print(paste(daymet("tmax", year, doy), "does not exist!"))
@@ -30,12 +30,17 @@ for(year in c(1990:1995,1999:2015)){
 			}
 			next
 		}else{
-			tmax <- read.tif("tmax", year, doy)
+		  tmax <- read.tif("tmax", year, doy)
 			tmin <- read.tif("tmin", year, doy)
-			tmean <- (tmax + tmin)/2
-			file <- paste0("tmean", year %% 100, formatC(doy, width = 3, format = "d", flag = "0"), ".tif")
-			writeRaster(tmean, filename=paste0(infolder, file), datatype='INT4S', overwrite=TRUE)
-			print(doy)
+			if(extent(tmax) == extent(tmin)){
+				tmean <- (tmax + tmin)/2
+				file <- paste0("tmean", year %% 100, formatC(doy, width = 3, format = "d", flag = "0"), ".tif")
+				writeRaster(tmean, filename=paste0(infolder, file), datatype='INT4S', overwrite=TRUE)
+				print(doy)
+			}else{
+				print(paste("the extents do not match! Year", year, "DoY", doy))
+				next
+			}
 		}		
 	}
 	print(paste("processed",year,"..."))
