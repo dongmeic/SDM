2c7c00287f463de66c98d8d42cb64d012e14d605
dongmeijane1107@gmail.com <dongmeijane1107@gmail.com>
2018-10-15 08:56:32 -0700

    added codes to check if the file exists in daymet_tmean.R

--- R/daymet_tmean.R
+++ R/daymet_tmean.R
@@ -3,27 +3,40 @@ library(rgdal)
 install.packages("tiff", repos='http://cran.us.r-project.org')
 library(tiff)
 
-read.tif <- function(vtype, year, doy){
+daymet <- function(vtype, year, doy){
 	infolder <- paste0("/gpfs/projects/gavingrp/dongmeic/daymet/", year, "/", vtype, "/")
 	file <- paste0(vtype, year %% 100, formatC(doy, width = 3, format = "d", flag = "0"), ".tif")
-	indata <- paste0(infolder, file)
-	raster(indata)
+	paste0(infolder, file)
 }
 
-mainDir <- "/gpfs/projects/gavingrp/dongmeic/daymet/"
-subDir <- "/tmean"
+read.tif <- function(vtype, year, doy){
+	if(!file.exists(daymet(vtype, year, doy))){
+		print(paste(daymet(vtype, year, doy), "does not exist!"))
+	}else{
+		raster(daymet(vtype, year, doy))
+	}
+}
+
 ptm <- proc.time()
-for(year in 1997:2015){ # run 1996 mannually first
-	dir.create(file.path(mainDir,year,subDir), showWarnings = FALSE)
+for(year in c(1990:1995,1999:2015)){
 	infolder <- paste0("/gpfs/projects/gavingrp/dongmeic/daymet/", year, "/tmean/")
 	dir.create(file.path(infolder), showWarnings = FALSE)
-	for(doy in 1:365){
-		tmax <- read.tif("tmax", year, doy)
-		tmin <- read.tif("tmin", year, doy)
-		tmean <- (tmax + tmin)/2	
-		file <- paste0("tmean", year %% 100, formatC(doy, width = 3, format = "d", flag = "0"), ".tif")
-		writeRaster(tmean, filename=paste0(infolder, file), datatype='INT4S', overwrite=TRUE)
-		print(doy)
+	for(doy in 260:365){
+		if(!file.exists(daymet("tmax", year, doy)) | !file.exists(daymet("tmin", year, doy))){
+			if(!file.exists(daymet("tmax", year, doy)){
+				print(paste(daymet("tmax", year, doy), "does not exist!"))
+			}else{
+				print(paste(daymet("tmin", year, doy), "does not exist!"))
+			}
+			next
+		}else{
+			tmax <- read.tif("tmax", year, doy)
+			tmin <- read.tif("tmin", year, doy)
+			tmean <- (tmax + tmin)/2
+			file <- paste0("tmean", year %% 100, formatC(doy, width = 3, format = "d", flag = "0"), ".tif")
+			writeRaster(tmean, filename=paste0(infolder, file), datatype='INT4S', overwrite=TRUE)
+			print(doy)
+		}		
 	}
 	print(paste("processed",year,"..."))
 }
