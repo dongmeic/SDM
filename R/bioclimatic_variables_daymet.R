eefca2b882af87ef460d0c7fee47a769db9d8474
dongmeijane1107@gmail.com <dongmeijane1107@gmail.com>
2018-10-18 14:00:28 -0700

    get daily climate from daymet

--- R/bioclimatic_variables_daymet.R
+++ R/bioclimatic_variables_daymet.R
@@ -6,7 +6,7 @@ library(doParallel)
 library(foreach)
 registerDoParallel(cores=28)
 
-source("/gpfs/projects/gavingrp/dongmeic/climate-space/R/damian/getDailyStatsFromDaily.R")
+source("/gpfs/projects/gavingrp/dongmeic/climate-space/R/damian/getStatsFromDaily.R")
 inpath <- "/gpfs/projects/gavingrp/dongmeic/beetle/output/tables/"
 setwd(inpath)
 start_year <- 1995; end_year <- 2015; years <- start_year:end_year; nt <- length(years)
@@ -22,20 +22,19 @@ foreach(i = 1:nt)%dopar%{
 	tmin.df.2 <- read.csv(paste0(inpath, "tmin/tmin", years[i+1],".csv"))
 	tmean.df.1 <- read.csv(paste0(inpath, "tmean/tmean", years[i],".csv"))
 	tmean.df.2 <- read.csv(paste0(inpath, "tmean/tmean", years[i+1],".csv"))
-	df <- data.frame(winterTmin=double(), Ecs=double(), Ncs=double(), Acs=double(), drop0=double(),
-								 drop5=double(), drop10=double(), drop15=double(), drop20=double(), drop20plus=double(),
-								 max.drop=double(), ddAugJul=double(), ddAugJun=double(), min20=double(), min22=double(), 
-								 min24=double(), min26=double(), min28=double(),min30=double(), min32=double(), 
-								 min34=double(), min36=double(), min38=double(), min40=double())
-	df.d <- data.frame(tmx=double(), tmp=double(), tmn=double())
+	df <- data.frame(Lcs=double(), maxAugT=double(), summerT40=double(), winterTmin=double(), Ecs=double(), Ncs=double(), Acs=double(), drop0=double(),
+									 drop5=double(), drop10=double(), drop15=double(), drop20=double(), drop20plus=double(),
+									 max.drop=double(), ddAugJul=double(), ddAugJun=double(), min20=double(), min22=double(), 
+									 min24=double(), min26=double(), min28=double(),min30=double(), min32=double(), 
+									 min34=double(), min36=double(), min38=double(), min40=double())
 	for(j in 1:dim1){
-		df.d$tmx <- as.numeric(rbind(tmax.df.1[j,3:367],tmax.df.2[j,3:367]))
-		df.d$tmp <- as.numeric(rbind(tmean.df.1[j,3:367],tmean.df.2[j,3:367]))
-		df.d$tmn <- as.numeric(rbind(tmin.df.1[j,3:367],tmin.df.2[j,3:367]))
-		df[j,] <- get.daily.stats(years[i], df.d$tmx, df.d$tmp, df.d$tmn)
+		tmx <- c(as.numeric(tmax.df.1[j,3:367]), as.numeric(tmax.df.2[j,3:367]))
+		tmp <- c(as.numeric(tmean.df.1[j,3:367]), as.numeric(tmean.df.2[j,3:367]))
+		tmn <- c(as.numeric(tmin.df.1[j,3:367]), as.numeric(tmin.df.2[j,3:367]))
+		df[j,] <- get.daily.stats(years[i], tmx, tmp, tmn)
 	}
 	print(paste("got data from", years[i+1]))
 	write.csv(df, paste0("daily_climate/Daymet/daymet_bioclimatic_variables_",years[i+1],".csv"), row.names = FALSE)  
 }
-proc.time() - ptm
+proc.time() - pt
 print("all done!")
