05a3032deedf17f70a2170791b867bd16868d117
dongmeijane1107@gmail.com <dongmeijane1107@gmail.com>
2018-12-03 22:00:53 -0800

    input data tables for SDM

--- R/input_data_block.R
+++ R/input_data_block.R
@@ -1,3 +1,27 @@
+# Created by Dongmei Chen
+# combine SDM input data tables; based on reorganize_data_tables.R
+
+library(parallel)
+library(doParallel)
+library(foreach)
+registerDoParallel(cores=28)
+
+# input and output path
+path <- "/gpfs/projects/gavingrp/dongmeic/beetle/output/tables"
+setwd(path)
+
+years <- 1996:2015; nyr <- length(years)
+# beetle presence data
+btlprs <- read.csv("beetle_presence.csv")
+bd <- btlprs[btlprs$allyears==1,]
+btlsum9 <- read.csv("ts_presence_sum9.csv")
+# location data
+loc <- read.csv("location.csv")
+# vegetation data
+tree <- read.csv("stand_age_density.csv")
+
+
+
 rescale <- function(x, x.min, x.max, n.divs) {
   range <- x.max - x.min + 0.0000001
   dist <- x - x.min
