fae7fbcdde84b257f84adc5b3ffa2ba7611262dd
dongmeijane1107@gmail.com <dongmeijane1107@gmail.com>
2018-12-30 14:52:58 -0800

    updated input data and logistic regression solver

--- R/input_data_block.R
+++ R/input_data_block.R
@@ -45,6 +45,7 @@ foreach (i=3:nyr)%dopar%{
   colnames(df)[12:13] <- c("sum9_t1","sum9_t2")
   df$year <- rep(years[i], dim(df)[1])
   df$vgt <- ifelse(df$btl_t==1 & df$vgt==0, 1, df$vgt)
+  df$sum9_diff <- df$sum9_t1 - df$sum9_t2
   bioclm <- combine_CRU_Daymet(i)[,1:61]
   df <- cbind(df[rows$rows,], bioclm)
   df <- cbind(subset(df, select=c("btl_t")), df[ , -which(colnames(df) %in% c("btl_t"))])
